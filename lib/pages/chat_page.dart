ba6f92a9378d7dc166d451161d4e3138358468bd
David Valero <dvdovni@gmail.com>
2025-01-21 22:56:07 -0500

    Caja de texto de nuestro chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +44,57 @@ class ChatPage extends StatelessWidget {
           ),
           SafeArea(
             child: Container(
-              color: Colors.white,
-              height: 50,
+                color: Colors.white,
+                // height: 50,
+                child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 8),
+                    child: _inputChat())),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _inputChat() {
+    return Container(
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                  hintText: 'Enviar mensaje', border: InputBorder.none),
             ),
           ),
+          // Botón de enviar
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            child: IconTheme(
+              data: IconThemeData(color: Colors.blue[400]),
+              child: Platform.isIOS
+                  ? CupertinoButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: const Text('Enviar',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        print(_textController.text);
+                        _textController.clear();
+                        FocusScope.of(context).requestFocus();
+                      },
+                    )
+                  : IconButton(
+                      icon: const Icon(Icons.send),
+                      onPressed: () {
+                        print(_textController.text);
+                        _textController.clear();
+                        FocusScope.of(context).requestFocus();
+                      },
+                    ),
+            ),
+          )
         ],
       ),
     );
