3f7745301e75ff415336bec640bc8c5abc45b947
David Valero <dvdovni@gmail.com>
2025-01-21 22:20:29 -0500

    ChatPage - Inicio

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,8 +6,40 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
+      appBar: AppBar(
+          title: const Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child:
+                Text('Da', style: TextStyle(color: Colors.white, fontSize: 12)),
+          ),
+          SizedBox(width: 10),
+          Text('David Valero')
+        ],
+      )),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: 100,
+              itemBuilder: (_, i) => ListTile(
+                title: Text('Item $i'),
+              ),
+              reverse: true,
+            ),
+          ),
+          Divider(
+            height: 1,
+          ),
+          SafeArea(
+            child: Container(
+              color: Colors.white,
+              height: 50,
+            ),
+          ),
+        ],
       ),
     );
   }
